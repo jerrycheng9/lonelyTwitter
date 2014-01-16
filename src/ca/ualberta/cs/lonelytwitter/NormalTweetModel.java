b5456198fd739386794fbdb0e2b02904a5e74f91
jcheng1 <jcheng1@ualberta.ca>
2014-01-16 16:30:09 -0700

    Add NormalTweetModel

---
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class NormalTweetModel extends LonelyTweetModel {
+
+	public NormalTweetModel(String text) {
+		super(text);
+	}
+
+	@Override
+	public Date getTimestamp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isimportant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
