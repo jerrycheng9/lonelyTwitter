a092146b2af010dcbd4f60cb94da0d1ad202a25b
jcheng1 <jcheng1@ualberta.ca>
2014-01-30 16:48:28 -0700

    Add new thing taught on lab

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,14 +1,19 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-
-import ca.ualberta.cs.lonelytwitter.*;
-import java.lang.reflect.Type;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Date;
 
+import com.google.gson.Gson;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,61 +23,89 @@ import android.widget.ListView;
 
 public class LonelyTwitterActivity extends Activity {
 
-	private Gson gson = new Gson();
-	private String json;
-	private EditText bodyText;
-	private ListView oldTweetsList;
-	protected ArrayList<ImportantTweetModel> inputtweets = new ArrayList<ImportantTweetModel>();
-	
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+        private static final String FILENAME = "file.sav";
+        private EditText bodyText;
+        private ListView oldTweetsList;
+        private ArrayList<AbstractTweetModel> tweet_models;
+        private ArrayAdapter<String> adapter;
+        private Gson gson=new Gson();
+        
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.main);
 
-		bodyText = (EditText) findViewById(R.id.body);
-		Button saveButton = (Button) findViewById(R.id.save);
-		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+                bodyText = (EditText) findViewById(R.id.body);
+                Button saveButton = (Button) findViewById(R.id.save);
+                oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
-		saveButton.setOnClickListener(new View.OnClickListener() {
+                saveButton.setOnClickListener(new View.OnClickListener() {
 
-			public void onClick(View v) {
-				setResult(RESULT_OK);
-				String text = bodyText.getText().toString();
-				ImportantTweetModel tweetModel = new ImportantTweetModel(text, new Date(System.currentTimeMillis()));
-				saveInFile(tweetModel);
-				String[] tweets = loadFromFile();
-				ArrayAdapter<String> adapter = new ArrayAdapter<String>(LonelyTwitterActivity.this,R.layout.list_item, tweets);
-				oldTweetsList.setAdapter(adapter);
-				adapter.notifyDataSetChanged();
-			}
-		});
-	}
+                        public void onClick(View v) {
+                                setResult(RESULT_OK);
+                                String text = bodyText.getText().toString();
+                                AbstractTweetModel New_tweet = new ImportantTweetModel(text, new Date(System.currentTimeMillis()));
+                                
+                                saveInFile(text, new Date(System.currentTimeMillis()),New_tweet);
+                                //finish();
+                                onStart();
 
-	@Override
-	protected void onStart() {
-		// TODO Auto-generated method stub
-		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				R.layout.list_item, tweets);
-		oldTweetsList.setAdapter(adapter);
-		
-		
-	}
+                        }
+                });
+        }
 
-	private String[] loadFromFile() {
-		ArrayList<ImportantTweetModel> tweets = new ArrayList<ImportantTweetModel>();
-		Type type = new TypeToken<ArrayList<ImportantTweetModel>>(){}.getType();
-		ArrayList<ImportantTweetModel> outputtweets = gson.fromJson(json, type);
-		for (ImportantTweetModel tweet : outputtweets){
-			tweets.add(tweet);
-		}
-		return tweets.toArray(new String[tweets.size()]);
-	}
-	
-	private void saveInFile(ImportantTweetModel tweetModel) {
-			inputtweets.add(tweetModel);
-		    json = gson.toJson(inputtweets);
-	}
+        @Override
+        protected void onStart() {
+                // TODO Auto-generated method stub
+                super.onStart();
+                AbstractTweetModel[] tweets = loadFromFile();
+                ArrayList<String> items=new ArrayList<String>();
+                for(AbstractTweetModel c : tweets){
+                        items.add(new String(c.getTimestamp().toString()+" | "+c.getText()));
+                }
+                adapter = new ArrayAdapter<String>(this,
+                                R.layout.list_item,items.toArray(new String[items.size()]));
+                oldTweetsList.setAdapter(adapter);
+        }
+
+        private AbstractTweetModel[] loadFromFile() {
+                tweet_models = new ArrayList<AbstractTweetModel>();
+                try {
+                        FileInputStream fis = openFileInput(FILENAME);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+                        String line = in.readLine();
+                        while (line != null) {
+                                tweet_models.add(gson.fromJson(line,ImportantTweetModel.class));
+                                line = in.readLine();
+                        }
+                        fis.close();
+                        in.close();
+                        
+                } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                return tweet_models.toArray(new AbstractTweetModel[tweet_models.size()]);
+        }
+        
+        private void saveInFile(String text, Date date,AbstractTweetModel ltm) {
+                try {
+                        FileOutputStream fos = openFileOutput(FILENAME,Context.MODE_APPEND);
+                        OutputStreamWriter fout = new OutputStreamWriter(fos);
+                        fout.write(new String(gson.toJson(ltm)+'\n'));
+                        fout.close();
+                        fos.close();
+                        
+                } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+        }
 }
